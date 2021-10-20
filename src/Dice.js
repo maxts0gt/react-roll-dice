5d6d940a51f729df0b243d5d747b3cf0114d9e58
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-21 08:14:29 +0900

    clean roll and dice

--- src/Dice.js
+++ src/Dice.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import './Dice.css';
 
 class Dice extends Component {
-	constructor(props) {
-		super(props);
-		// this.props = { randomDice: 'one' };
-	}
+
 	render() {
 		return <i className={`fas fa-dice-${this.props.randomDice} ${this.props.text}`}></i>;
 	}
--- src/Roll.js
+++ src/Roll.js
@@ -3,35 +3,29 @@ import Dice from './Dice.js';
 import './Roll.css';
 
 class Roll extends Component {
+	static defaultProps = {
+		sides: ['one', 'two', 'three', 'four', 'five', 'six'],
+	};
 	constructor(props) {
 		super(props);
-		this.state = { text: 'Roll' };
+		this.state = { text: 'Roll', die1: 'one', die2: 'two' };
 		this.handleClick = this.handleClick.bind(this);
-		this.dices = ['one', 'two', 'three', 'four', 'five', 'six'];
-		this.randomDiceOne = this.dices[Math.floor(Math.random() * this.dices.length)];
-		this.randomDiceTwo = this.dices[Math.floor(Math.random() * this.dices.length)];
 	}
 
 	handleClick(e) {
 		this.setState({ text: 'Rolling...' });
 		setTimeout(() => {
-			this.randomDiceOne = this.dices[Math.floor(Math.random() * this.dices.length)];
-			this.randomDiceTwo = this.dices[Math.floor(Math.random() * this.dices.length)];
-			this.setState({ text: 'Roll' });
+			const newDieOne = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+			const newDieTwo = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+			this.setState({ text: 'Roll', die1: newDieOne, die2: newDieTwo });
 		}, 1000);
 	}
 	render() {
 		return (
 			<div>
 				<div>
-					<Dice
-						text={this.state.text === 'Rolling...' ? 'Rolling' : 'Roll'}
-						randomDice={this.randomDiceOne}
-					/>
-					<Dice
-						text={this.state.text === 'Rolling...' ? 'Rolling' : 'Roll'}
-						randomDice={this.randomDiceTwo}
-					/>
+					<Dice text={this.state.text === 'Rolling...' ? 'Rolling' : 'Roll'} randomDice={this.state.die1} />
+					<Dice text={this.state.text === 'Rolling...' ? 'Rolling' : 'Roll'} randomDice={this.state.die2} />
 				</div>
 				<button className='btn btn-dark' onClick={this.handleClick}>
 					{this.state.text}
@@ -41,4 +35,4 @@ class Roll extends Component {
 	}
 }
 
-export default Roll;
+export default Roll; 
